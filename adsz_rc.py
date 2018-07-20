32b7d95b92cd365cdbee9ae006a503f5951c16c4
Adam Szady <adsz@beit.tech>
2018-07-20 07:51:15 -0400

    Print charts with higher dpi

--- adsz_rc.py
+++ adsz_rc.py
@@ -56,19 +56,19 @@ INIT_ARR[0] = 1.
 # plt.bar(range(len(REF_DISTRIBUTION)), REF_DISTRIBUTION)
 # plt.show()
 
-def BRX1(i): return lambda A: RX1(i)
+def BRX1(N): return lambda A: [RX1(i) for i in range(N)]
 def BRX2(i): return lambda A: RX2(i)
 def BRX3(i): return lambda A: RX3(i)
-def BRZZ(i): return lambda A: RZZ(next(A), i)
+def BRZZ(N): return lambda A: [RZZ(next(A), i) for i in range(N)]
 def BCZ(i, j): return lambda A: CZ(i, j)
 
 def BA5(i, j): return lambda A: [RZZ(next(A), i), RZZ(next(A), j), RX1(i), RX1(j), CZ(i, j)]
 
 BUILDING_BLOCKS = dict(\
-    [('RX1.{}'.format(i), BRX1(i)) for i in range(N)] +\
+    [('RX1', BRX1(N))] +\
     #[('RX2.{}'.format(i), BRX2(i)) for i in range(N)] +\
     #[('RX3.{}'.format(i), BRX3(i)) for i in range(N)] +\
-    [('RZZ.{}'.format(i), BRZZ(i)) for i in range(N)] +\
+    [('RZZ', BRZZ(N))] +\
     [('CZ.{}.{}'.format(i, j), BCZ(i, j))
       for i in range(N) for j in range(N) if (i,j) in TOPO] +\
     [('A5.{}.{}'.format(i, j), BA5(i, j))
@@ -102,9 +102,9 @@ dir = os.path.join(os.path.dirname(__file__), 'adsz_runs', PROJECT_NAME)
 os.makedirs(dir, exist_ok=True)
 
 DATA = [{
-    'seq': ['RX1.{}'.format(i) for i in range(N)] +\
-           ['RZZ.{}'.format(i) for i in range(N)] +\
-            ['RX1.{}'.format(i) for i in range(N)] +\
+    'seq': ['RX1'] +\
+           ['RZZ'] +\
+            ['RX1'] +\
             ['A5.{}.{}'.format(i,j) for i in range(N) for j in range(N) if (i,j) in TOPO if i<j]
     ,
     'score': 10000
@@ -114,7 +114,7 @@ best_score = 1000
 best_count = 0
 
 while True:
-    DATA = sorted(DATA, key=lambda x: (x['score'] + len(x['seq'])*0.05))[:20]
+    DATA = sorted(DATA, key=lambda x: (x['score'] + len(x['seq'])*0.01))[:20]
 
     prev = random.choices(DATA, weights=np.linspace(1, 0, len(DATA)))[0]
 
@@ -178,9 +178,9 @@ while True:
         probs = np.square(np.abs(wf.amplitudes))
         plt.bar(range(len(REF_DISTRIBUTION)), REF_DISTRIBUTION, width=0.8)
         plt.bar(range(len(REF_DISTRIBUTION)), probs, width=0.6)
-        plt.bar(range(len(REF_DISTRIBUTION)), artificial_sample(probs, samples=100), width=0.2)
+        #plt.bar(range(len(REF_DISTRIBUTION)), artificial_sample(probs, samples=100), width=0.2)
         plt.title('-'.join(ne['seq']))
-        plt.savefig(os.path.join(dir, '{}-chart1.png'.format(best_count)))
+        plt.savefig(os.path.join(dir, '{}-chart1.png'.format(best_count)), dpi=300)
 
         H = np.array(task.history)
         Hmin = np.minimum.accumulate(H)
@@ -191,6 +191,11 @@ while True:
         ax.plot(range(len(task.history)), Hmin, linewidth=1, color='black')
         ax.set_yscale('log')
         fig.savefig(os.path.join(dir, '{}-chart2.png'.format(best_count)))
+
+        with open(os.path.join(dir, '{}-recipe.txt'.format(best_count)), 'wt') as f:
+            f.write(str('-'.join(ne['seq'])))
+            f.write('\n')
+            f.write(str(ne['score']))
 
         with open(os.path.join(dir, '{}-log.txt'.format(best_count)), 'wt') as f:
             f.write(str(p_best))
