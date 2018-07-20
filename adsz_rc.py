9c604916f03e28a873dc18c076d23b18b8f1d45b
Adam Szady <adsz@beit.tech>
2018-07-19 20:38:08 -0400

    Add topology into rc

--- adsz_rc.py
+++ adsz_rc.py
@@ -33,12 +33,13 @@ parser.add_argument('rows', type=int, default=2)
 parser.add_argument('cols', type=int, default=2)
 parser.add_argument('-k', type=int, default=3)
 parser.add_argument('-seq', type=str)
+parser.add_argument('-t', type=str, default='all')
 args = parser.parse_args()
 
 ROWS, COLS = args.rows, args.cols
 K = args.k
-
 N = ROWS*COLS
+TOPO = topology(N, args.t)
 
 REF_DISTRIBUTION = bs_dist(ROWS, COLS)
 
@@ -61,13 +62,18 @@ def BRX3(i): return lambda A: RX3(i)
 def BRZZ(i): return lambda A: RZZ(next(A), i)
 def BCZ(i, j): return lambda A: CZ(i, j)
 
+def BA5(i, j): return lambda A: [RZZ(next(A), i), RZZ(next(A), j), RX1(i), RX1(j), CZ(i, j)]
+
 BUILDING_BLOCKS = dict(\
     [('RX1.{}'.format(i), BRX1(i)) for i in range(N)] +\
-    [('RX2.{}'.format(i), BRX2(i)) for i in range(N)] +\
-    [('RX3.{}'.format(i), BRX3(i)) for i in range(N)] +\
+    #[('RX2.{}'.format(i), BRX2(i)) for i in range(N)] +\
+    #[('RX3.{}'.format(i), BRX3(i)) for i in range(N)] +\
     [('RZZ.{}'.format(i), BRZZ(i)) for i in range(N)] +\
     [('CZ.{}.{}'.format(i, j), BCZ(i, j))
-      for i in range(N) for j in range(N) if i!=j])
+      for i in range(N) for j in range(N) if (i,j) in TOPO] +\
+    [('A5.{}.{}'.format(i, j), BA5(i, j))
+      for i in range(N) for j in range(N) if (i, j) in TOPO]
+    )
 
 
 def build_bb(*bb_seq):
@@ -91,17 +97,24 @@ def count_vars_bb(*bb_seq):
             ]
     return next(A)
 
+PROJECT_NAME = str(args.t.lower()) + '-' + str(ROWS) + '-' + str(COLS) + '/RC/' + str(int(time.time()))
+dir = os.path.join(os.path.dirname(__file__), 'adsz_runs', PROJECT_NAME)
+os.makedirs(dir, exist_ok=True)
+
 DATA = [{
     'seq': ['RX1.{}'.format(i) for i in range(N)] +\
            ['RZZ.{}'.format(i) for i in range(N)] +\
-            ['RX1.{}'.format(i) for i in range(N)]
+            ['RX1.{}'.format(i) for i in range(N)] +\
+            ['A5.{}.{}'.format(i,j) for i in range(N) for j in range(N) if (i,j) in TOPO if i<j]
     ,
     'score': 10000
 }]
 
-while True:
-    DATA = sorted(DATA, key=lambda x: (x['score'], x['seq']))[:20]
+best_score = 1000
+best_count = 0
 
+while True:
+    DATA = sorted(DATA, key=lambda x: (x['score'] + len(x['seq'])*0.05))[:20]
 
     prev = random.choices(DATA, weights=np.linspace(1, 0, len(DATA)))[0]
 
@@ -116,8 +129,17 @@ while True:
         'seq': prev['seq'][:]
     }
 
-    p = random.randint(0, len(prev['seq']))
-    ne['seq'].insert(p, random.choice(list(BUILDING_BLOCKS.keys())))
+    action = 'add'
+    if len(ne['seq']) >= 1 and random.random() < 0.5:
+        action = 'del'
+
+    if action == 'add':
+        for z in range(5):
+            p = random.randint(0, len(ne['seq']))
+            ne['seq'].insert(p, random.choice(list(BUILDING_BLOCKS.keys())))
+    elif action == 'del':
+        p = random.randint(0, len(prev['seq']) - 1)
+        ne['seq'] = ne['seq'][:p] + ne['seq'][p+1:]
 
     test1 = build_bb(*ne['seq'])
     num_vars = count_vars_bb(*ne['seq'])
@@ -127,17 +149,55 @@ while True:
 
     task = OptTask(test1, REF_DISTRIBUTION)
 
+    result = None
     if num_vars == 0:
         print('No vars, calling directly')
         print(test1())
         ne['score'] = task([])
     else:
         result = differential_evolution(task, [(-1.3, +1.3)] * num_vars, init='random',
-            disp=False, maxiter=3, popsize=5, recombination=0.7,
+            disp=True, maxiter=3, popsize=5, recombination=0.7,
             strategy='best1bin', polish=USE_WAVEFUNCTION)
 
         ne['score'] = result.fun
 
     DATA.append(ne)
     print('Done')
+
+    if ne['score'] < best_score:
+        best_score = ne['score']
+        best_count += 1
+        print('New best score!')
+
+        p_best = test1(*result.x if num_vars > 0 else [])
+
+        qvm = QVMConnection()
+        wf = qvm.wavefunction(p_best)[0]
+
+        plt.cla()
+        probs = np.square(np.abs(wf.amplitudes))
+        plt.bar(range(len(REF_DISTRIBUTION)), REF_DISTRIBUTION, width=0.8)
+        plt.bar(range(len(REF_DISTRIBUTION)), probs, width=0.6)
+        plt.bar(range(len(REF_DISTRIBUTION)), artificial_sample(probs, samples=100), width=0.2)
+        plt.title('-'.join(ne['seq']))
+        plt.savefig(os.path.join(dir, '{}-chart1.png'.format(best_count)))
+
+        H = np.array(task.history)
+        Hmin = np.minimum.accumulate(H)
+        plt.cla()
+        fig = plt.gcf()
+        ax = fig.gca()
+        ax.scatter(range(len(task.history)), task.history, 2, 'red')
+        ax.plot(range(len(task.history)), Hmin, linewidth=1, color='black')
+        ax.set_yscale('log')
+        fig.savefig(os.path.join(dir, '{}-chart2.png'.format(best_count)))
+
+        with open(os.path.join(dir, '{}-log.txt'.format(best_count)), 'wt') as f:
+            f.write(str(p_best))
+            f.write('Compiled:\n')
+            try:
+                f.write(str(postoptimize(p_best)))
+            except Exception:
+                print('NOT writing compiled version :(')
+                pass
 
